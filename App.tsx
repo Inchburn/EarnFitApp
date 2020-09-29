7c9fc9b82afa66b7242447cbb2fef3ff581b002d
ShravanKumar <shravansantosh892007@gmail.com>
2020-09-29 10:04:58 +0530

    Bottom menu

--- App.tsx
+++ App.tsx
@@ -1,11 +1,65 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { AntDesign } from "@expo/vector-icons";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AntDesign name="home" size={24} color="black" />
+    </View>
+  );
+}
+
+function WalletScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AntDesign name="wallet" size={24} color="black" />
+    </View>
+  );
+}
+
+function AccountScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AntDesign name="idcard" size={24} color="black" />
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to EarnFit app!</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: () => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = "home";
+            } else if (route.name === "Wallet") {
+              iconName = "wallet";
+            } else if (route.name === "Account") {
+              iconName = "idcard";
+            }
+
+            // You can return any component that you like here!
+            return <AntDesign name={iconName} size={24} color="black" />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "teal",
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Wallet" component={WalletScreen} />
+        <Tab.Screen name="Account" component={AccountScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
